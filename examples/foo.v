2015f9bb653de6b407c6b247b3bfdf785a4ed59f
theostos <theo.stoskopf@proton.me>
2025-12-16 16:45:52 +0100

    Add test for list_notations_in_statement related to scope

--- examples/foo.v
+++ examples/foo.v
@@ -1,5 +1,6 @@
 From Coq Require Import Lia.
 From Coq Require Import ssreflect ssrbool.
+
 Theorem t:
     forall n: nat, 1 + n > n.
 Proof.
@@ -13,4 +14,20 @@ Proof. by elim: n => //= ? ->. Qed.
 Lemma addnAC n m l : n + m + l = m + (n + l).
 Proof.
   by elim : n => //= ? ->.
+Qed.
+
+Notation "x ⊕ y" := (x + y) (at level 50, left associativity).
+
+Lemma use_notation_wo_scope : 1 ⊕ 2 = 3.
+Proof.
+  reflexivity.
+Qed.
+
+Declare Scope my_scope.
+Notation "x ⊕_s y" := (x + y) (at level 50, left associativity): my_scope.
+Open Scope my_scope.
+
+Lemma use_notation_w_scope : 1 ⊕_s 2 = 3.
+Proof.
+  reflexivity.
 Qed.
