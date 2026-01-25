44eb316c8f9792d474a66a7f2271278bcf4ade4b
theostos <theo.stoskopf@proton.me>
2026-01-25 13:21:02 +0100

    Add SessionResponse subclass

--- pytanque/response.py
+++ pytanque/response.py
@@ -42,6 +42,9 @@ class AnyResponse:
 class SessionResponse(BaseResponse):
     def extract_response(self) -> State:
         return State.from_json(self.to_json())
+    
+    def extract_state(self) -> State:
+        return State.from_json(self.to_json())
 
 class StartResponse(BaseStartResponse, SessionResponse):
     pass
